a1342b348888d5ba4030cc127c33f0d00f1877d3
Mayank <mayankag.professional@proton.me>
2025-12-22 14:42:18 +0530

    Updated Controller Code

--- UserProfileService/src/main/java/info/mayankag/UserProfileService/controller/UserController.java
+++ UserProfileService/src/main/java/info/mayankag/UserProfileService/controller/UserController.java
@@ -5,6 +5,7 @@ import info.mayankag.UserProfileService.dto.*;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
@@ -30,20 +31,26 @@ public class UserController {
 
     @PostMapping({"/register"})
     @Operation(summary = "Register User", description = "Register New User")
-    public ResponseEntity<RegisterResponseDto> register(@RequestBody RegisterRequestDto registerRequestDto) {
+    public ResponseEntity<RegisterResponseDto> register(
+            @RequestBody @Valid RegisterRequestDto registerRequestDto) {
 
         // Register User and return the boolean status
         Optional<String> errorMessage = userService.registerUser(registerRequestDto);
 
         // Return response based on the status received
         return errorMessage.map(
-                s -> ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new RegisterResponseDto(s))).
-                orElseGet(() -> ResponseEntity.ok(new RegisterResponseDto("User registered successfully!")));
+                s -> ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body(new RegisterResponseDto(s))).
+                orElseGet(() -> ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body(new RegisterResponseDto("User registered successfully!")));
     }
 
     @PostMapping("/login")
     @Operation(summary = "Login User", description = "Login Existing User")
-    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginInputDto) {
+    public ResponseEntity<LoginResponseDto> login(
+            @RequestBody @Valid LoginRequestDto loginInputDto) {
 
         // Check credentials and return JWT Token if present
         Optional<String> token = userService.loginUser(loginInputDto);
@@ -67,7 +74,7 @@ public class UserController {
         }
 
         return userService.validateToken(authHeader.substring(7))
-                ? ResponseEntity.ok().build()
+                ? ResponseEntity.noContent().build()
                 : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
