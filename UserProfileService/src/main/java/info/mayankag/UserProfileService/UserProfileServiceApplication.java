04ea72185c66608ad96ad4a42613e9b8a3f34e31
Mayank <mayankag.professional@proton.me>
2025-12-22 22:33:21 +0530

    Solved Serialization Issue for Redis

--- UserProfileService/src/main/java/info/mayankag/UserProfileService/UserProfileServiceApplication.java
+++ UserProfileService/src/main/java/info/mayankag/UserProfileService/UserProfileServiceApplication.java
@@ -2,8 +2,14 @@ package info.mayankag.UserProfileService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class UserProfileServiceApplication {
 
 	public static void main(String[] args) {
